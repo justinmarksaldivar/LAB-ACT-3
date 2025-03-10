24c6806d4dafacc7cfed6cafafbc6080ac4a27b6
justinmarksaldivar <justinmark.saldivar0916@gmail.com>
2025-03-10 23:31:10 +0800

    Add files via upload

---
+++ NUMBER.py
@@ -0,0 +1,8 @@
+def check_number():
+    
+    num = int(input("Number:"))
+    if num % 2 == 0:
+        print(num, "is an Even Number")
+    else:
+        print(num, "is an Odd Number")
+check_number()
